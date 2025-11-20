fd5358ab92c3c7d2db731efeec36ede8c75c8788
rohan-tech-negi <rohanwork953@gmail.com>
2025-11-20 13:27:08 +0530

    complete setup of the user.models including the pre hook method and the instance

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-
+import bcrypt from "bcryptjs"
 const userSchema = new mongoose.Schema({
     fullName:{
         type:String,
@@ -48,5 +48,24 @@ const userSchema = new mongoose.Schema({
 }, {timestamps:true})
 
 const User = mongoose.model("User", userSchema);
+
+// pre hook
+userSchema.pre("save", async function (next) {
+
+    if(!this.isModified("password")) return next()
+    try {
+        const salt  = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
+  return isPasswordCorrect;
+};
 
 export default User;
