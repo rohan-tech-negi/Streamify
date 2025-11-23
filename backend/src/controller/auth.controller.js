eecf904c0ed29dcc1f148c8414ef05eaa091ad12
rohan-tech-negi <rohanwork953@gmail.com>
2025-11-23 18:26:51 +0530

    fixes some bugs from the login controller

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -49,39 +49,42 @@ export async function signup(req,res){
     }
 }
 
-export async function login(req,res){
-    try {
-        const {email, password} = req.body();
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
 
-        if(!email || !password){
-            return res.status(400).json({message:"All fields are required"})
-        }
-
-        const user = await User.findOne({email});
-        if(!user) return res.status(404).json({message:"Invaid email or password"})
-
-        const isPasswordCorrect = await user.matchPassword(password)
-        if(!isPasswordCorrect) return res.status(401).json({message:"Invalid password"})
-
-
-        const token = jwt.sign({userId:newUser._id}, process.env.JWT_SECRET_KEY,{
-            expiresIn: "7d"
-         })
-
-         res.cookie("jwt", token, {
-            maxAge: 7 * 24 * 60 *60 *1000,
-            httpOnly:true,
-            sameSite:"strict",
-            secure: process.env.NODE_ENV === "production"
-         })
-
-
-         res.status(200).json({success:true, user})
-    } catch (error) {
-        console.group("Error in loogin controler", error.message)
-        res.status(500).json({message:"Internal server error"})
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ success: true});
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
+
 
 
 export function logout(req,res){
