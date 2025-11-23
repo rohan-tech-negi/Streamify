c9e12c06988fbdee0f11b19a99675ee46b7296d1
rohan-tech-negi <rohanwork953@gmail.com>
2025-11-23 18:17:37 +0530

    completed the logout controller also

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -85,5 +85,6 @@ export async function login(req,res){
 
 
 export function logout(req,res){
-    res.send("logout route")
+    res.clearCookie("jwt")
+    res.status(200).json({success:true, message:"Logout successful"})
 }
