296984eb30736875a3b829bf682a164df088c332
rohan-tech-negi <rohanwork953@gmail.com>
2025-11-24 13:53:50 +0530

    created another end point for the controllers and setting up other things

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
-
+import { getMyfriends, getRecommendedUsers , sendFriendRequest } from "../controller/user.controller.js"
 
 const router = express.Router()
 
@@ -9,5 +9,6 @@ router.use(protectRoute)
 
 router.get("/",getRecommendedUsers)
 router.get("/friends", getMyfriends)
+router.post("/friend-request/:id", sendFriendRequest)
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 
+
 app.listen(PORT,()=>{
     console.log("the app is up on port 5000")
 })
