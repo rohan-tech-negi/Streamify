8b300f3db8a785e91f7bed043a1f13c494709744
rohan-tech-negi <rohanwork953@gmail.com>
2025-11-24 12:42:45 +0530

    did some changes in the env

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,7 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/user.js"
 import jwt from "jsonwebtoken"
+
 export async function signup(req,res){
     const{email, password, fullName} = req.body
 
@@ -30,6 +32,19 @@ export async function signup(req,res){
       password,
       profilePic: randomAvatar,
     });
+
+
+
+        try {
+          await upsertStreamUser({
+            id:newUser._id.toString(),
+            name: newUser.fullName,
+            image:newUser.profilePic || "",
+        })
+        console.log(`Stream user created${newUser.fullName}`)
+        } catch (error) {
+          console.log("Error in the stream connection", error)
+        }
 
          const token = jwt.sign({userId:newUser._id}, process.env.JWT_SECRET_KEY,{
             expiresIn: "7d"
--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -13,6 +13,16 @@ if(!apiKey || !apiSecret){
 const streamClient = StreamChat.getInstance(apiKey,apiSecret);
 
 
-export const createStreamUser = async (userData) => {
-    
+export const upsertStreamUser = async (userData) => {
+    try {
+        await streamClient.upsertUsers([userData]);
+        return userData;
+    } catch (error) {
+        console.log("Error upserting stream user:", error)
+    }
 }
+
+
+
+
+// 
