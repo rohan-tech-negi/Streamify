80e7e3e8c70ba7aa6920ad25d37412fd3e33f911
rohan-tech-negi <rohanwork953@gmail.com>
2025-11-26 00:34:57 +0530

    done with the chat controller and setup

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -23,6 +23,13 @@ export const upsertStreamUser = async (userData) => {
 }
 
 
-
+export const generateStreamToken = (userId) =>{
+    try {
+        const userIdStr = userId.toString();
+        return streamClient.createToken(userIdStr)
+    } catch (error) {
+        console.log("Error generating stream token", error)
+    }
+}
 
 // 
