5cce593b2bf47a3e4f3d1ce2acfe378e7019d8c1
rohan-tech-negi <rohanwork953@gmail.com>
2025-11-29 14:38:17 +0530

    setup all the routes and all are working fine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import {Routes} from "react-router"
+import {Route, Routes} from "react-router"
+import HomePage from "./pages/HomePage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import NotificationPage from "./pages/NotificationPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
 const App = () => {
   return (
     <div className='flex  h-screen justify-center items-center' data-theme="coffee">
     
     <Routes>
-
-
+      <Route path='/' element={<HomePage></HomePage>}></Route>
+      <Route path='/signup' element={<SignUpPage></SignUpPage>}></Route>
+      <Route path='/login' element={<LoginPage></LoginPage>}></Route>
+      <Route path='/notification' element={<NotificationPage></NotificationPage>}></Route>
+      <Route path='/call' element={<CallPage></CallPage>}></Route>
+      <Route path='/chat' element={<ChatPage></ChatPage>}></Route>
+      <Route path='/onboarding' element={<OnboardingPage></OnboardingPage>}></Route>
     </Routes>
 
     </div>
