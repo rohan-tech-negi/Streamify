3dd415e2cbcc56651afc618c38cc5d1e4aabf56a
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-02 14:39:22 +0530

    signup page working

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import {ShipWheelIcon} from "lucide-react"
 const SignUpPage = () => {
   const[signupData, setSignupData] = useState({
     fullName:"",
@@ -10,8 +10,27 @@ const SignUpPage = () => {
   const handleSignup = (e) =>{
     e.preventDefault()
   }
+
+
   return (
-    <div>SignUpPage</div>
+    <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest">
+        <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+
+          {/* signup form - left side */}
+          <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+            {/* logo */}
+            <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              Streamify
+            </span>
+          </div>
+          </div>
+
+        </div>
+
+    </div>
   )
 }
 
