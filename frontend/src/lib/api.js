4cafc4074efed77f7286e098f8945bba35dbe855
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-05 23:40:22 +0530

    as continuing the work making a new some middleware kind of thing for the onboarding page

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -11,3 +11,8 @@ export const getAuthUser = async()=>{
     
       return res.data;
     }
+
+
+export const completeOnboarding = async()=>{
+  
+}
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import useAuthUser from '../hooks/useAuthUser'
 import { useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import {toast} from "react-hot-toast"
 
 const OnboardingPage = () => {
   const {authUser} = useAuthUser()
+  const queryClient = useQueryClient()
 
     const [formState, setFormState] = useState({
     fullName: authUser?.fullName || "",
@@ -13,7 +16,15 @@ const OnboardingPage = () => {
     location: authUser?.location || "",
     profilePic: authUser?.profilePic || "",
   });
-  
+
+  const {mutation:onboardingMutation, isPending} = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: ()=>{
+      toast.success("profile onboarded successfully");
+      queryClient.invalidateQueries({queryKey: ["authUser"]})
+    }
+  })
+
   return (
     <div>OnboardingPage</div>
   )
