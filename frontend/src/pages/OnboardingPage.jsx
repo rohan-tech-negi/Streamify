ef0d4109f66d92a319508482eb261d6fa40dd496
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-05 23:49:31 +0530

    continuing with the ui

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -3,6 +3,7 @@ import useAuthUser from '../hooks/useAuthUser'
 import { useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import {toast} from "react-hot-toast"
+import { completeOnboarding } from '../lib/api'
 
 const OnboardingPage = () => {
   const {authUser} = useAuthUser()
@@ -25,8 +26,26 @@ const OnboardingPage = () => {
     }
   })
 
+   const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onboardingMutation(formState);
+  };
+
+
+
   return (
-    <div>OnboardingPage</div>
+    <div className='min-h-screen bg-base-100 flex items-center justify-center p-4'>
+      <div className='card bg-base-200 w-full max-w-3xl shadow-xl'>
+        <div className='card-body p-6 sm:p-8'>
+          <h1 className='text-2xl sm:text-3xl font-bold text-center mb-6'>Complete your profile</h1>
+        </div>
+          <form onSubmit={handleSubmit} className='space-y-6'>
+
+          </form>
+      </div>
+
+    </div>
   )
 }
 
