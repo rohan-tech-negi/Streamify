5314f220182d0cefb4892bcbaeef847a0fbdb145
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-05 23:34:37 +0530

    started the work for the onboardingpage

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import useAuthUser from '../hooks/useAuthUser'
+import { useState } from 'react'
 
 const OnboardingPage = () => {
+  const {authUser} = useAuthUser()
+
+    const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+  
   return (
     <div>OnboardingPage</div>
   )
