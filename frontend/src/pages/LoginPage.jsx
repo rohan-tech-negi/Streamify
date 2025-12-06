913bc390078bc2cdb6fc43d254dc8079acbe9d22
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-06 20:27:15 +0530

    started my work with the loginpage by making all the fields out of it

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {useMutation, useQueryClient} from "@tanstack/react-query"
-
+import {ShipWheelIcon} from "lucide-react"
 const LoginPage = () => {
   const[loginData, setLoginData] = useState({
     email: "",
@@ -18,9 +18,66 @@ const LoginPage = () => {
       e.preventDefault();
       loginMutation(loginData)
   }
-  return (
-    <div className=''>
 
+  const handleSubmit=()=>{
+
+  }
+  return (
+    <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme="forest">
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+
+
+          {/* login form selection */}
+          <div className='w-full lg:w-1/2 p-4 sm:p-8 flex flex-col'>
+            {/* logo */}
+            <div className='mb-4 flex items-center justify-start gap-2'>
+                <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+              Streamify
+            </span>
+            </div>
+
+            {/* error message display */}
+            {error && (
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
+
+
+          <div className='w-full'>
+              <form onSubmit={handleSubmit}>
+                <div className='space-y-4'>
+                  <div>
+                    <h2 className='text-xl font-semibold'>Welcome Back</h2>
+                  <p className='text-sm opacity-70'>
+                    Sign in to your account to continue your language journey
+                  </p>
+                  </div>
+
+                  <div className='flex flex-col gap-3 '>
+                       <div className="form-control w-full space-y-2">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="hello@example.com"
+                      className="input input-bordered w-full"
+                      value={loginData.email}
+                      onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
+                      required
+                    />
+                  </div>
+                  </div>
+                  
+                </div>
+
+
+              </form>
+          </div>
+          </div>
+      </div>
     </div>
   )
 }
