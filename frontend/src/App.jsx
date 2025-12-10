fe0c3e943f2b4843b000209c86d26e62b66595ad
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-11 00:25:10 +0530

    starting working on the layout and the other components of the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 import {Navigate, Route, Routes} from "react-router"
 import HomePage from "./pages/HomePage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
@@ -8,6 +8,7 @@ import CallPage from "./pages/CallPage.jsx"
 import ChatPage from "./pages/ChatPage.jsx"
 import OnboardingPage from "./pages/OnboardingPage.jsx"
 import {Toaster} from "react-hot-toast"
+import Layout from "./components/Layout.jsx"
 // import { useQuery } from '@tanstack/react-query'
 // import axios from "axios"
 // import { axiosInstance } from './lib/axios.js'
@@ -32,7 +33,10 @@ const App = () => {
   path="/"
   element={
     isAuthenticated && isOnboarded ? (
-      <HomePage />
+      <Layout >
+        <HomePage />
+      </Layout>
+      
     ) : (
       <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
     )
