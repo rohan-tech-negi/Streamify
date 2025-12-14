90d172730ce7ae6b8d80472b833cb1d1f3f75657
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-14 12:41:00 +0530

    fixed the import error

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-
+import useThemeStore from '../store/useThemeStore'
+// import {useThemeStore} from "../store/useThemeStore.js"
 const HomePage = () => {
+
+  const{theme, setTheme} = useThemeStore() 
   return (
     <div>HomePage</div>
   )
