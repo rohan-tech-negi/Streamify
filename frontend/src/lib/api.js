945b410ec2555ddc689a9826aa05f6df96dac38a
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-14 13:20:21 +0530

    just setting up the homepage along side with the imported queries

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -31,3 +31,23 @@ export const login =async (loginData) =>{
     const response = await axiosInstance.post("/auth/login", loginData);
     return response.data;
 }
+
+export async function getUserFriends() {
+  const response = await axiosInstance.get("/users/friends");
+  return response.data;
+}
+
+export async function getRecommendedUsers() {
+  const response = await axiosInstance.get("/users");
+  return response.data;
+}
+
+export async function getOutgoingFriendReqs() {
+  const response = await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
+
+export async function sendFriendRequest(userId) {
+  const response = await axiosInstance.post(`/users/friend-request/${userId}`);
+  return response.data;
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-
-
+import React, { useState } from 'react'
+import {useQueryClient, useQuery, useMutation} from "@tanstack/react-query"
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from '../lib/api'
 // import {useThemeStore} from "../store/useThemeStore.js"
 const HomePage = () => {
+  const queryClient = useQueryClient()
+  const[outgoingRequestsIds, setOutgoingRequestsIds] = useState([])
 
+  const {data: friends=[], isLoading:loadingFriends} = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends
+  })
 
+  const {data:recommendedUsers=[], isLoading:loadingUsers} = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommendedUsers
+  })
+
+  const {data: outgoingFriendReqs} = useQuery({
+    queryKey:["outgoingFriendreqs"],
+    queryFn: getOutgoingFriendReqs,
+  })
+
+  const {mutate: sendRequestMutation, isPending} = useMutation({
+    mutationFn: sendFriendRequest,
+    onSuccess: ()=> queryClient.invalidateQueries({queryKey: ["outgoingFriendReqs"]}),
+  })
   return (
     <div>
 
