5c3a791e962bc54956dd94deedaa166db71cb841
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-14 14:08:21 +0530

    completed the friends cards section for the card layout of any friend joined to my account

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendR
 import {Link} from "react-router"
 import {UsersIcon} from "lucide-react"
 import FriendCard from '../components/FriendCard'
+import NoFriendsFound from '../components/NoFriendsFound'
 // import {useThemeStore} from "../store/useThemeStore.js"
 const HomePage = () => {
    const queryClient = useQueryClient();
@@ -55,7 +56,7 @@ const HomePage = () => {
             <span className="loading loading-spinner loading-lg" />
           </div>
         ) : friends.length === 0 ? (
-        <p>No friends yet</p>
+        <NoFriendsFound></NoFriendsFound>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {friends.map((friend) => (
