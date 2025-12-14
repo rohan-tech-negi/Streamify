4940d28e6a3c388f263e85f452290b99139479c2
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-14 13:32:43 +0530

    done with the backend setup of the homepage.jsx now moving on to the frontend part

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,32 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useQueryClient, useQuery, useMutation} from "@tanstack/react-query"
 import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from '../lib/api'
 // import {useThemeStore} from "../store/useThemeStore.js"
 const HomePage = () => {
-  const queryClient = useQueryClient()
-  const[outgoingRequestsIds, setOutgoingRequestsIds] = useState([])
+   const queryClient = useQueryClient();
+  const [outgoingRequestsIds, setOutgoingRequestsIds] = useState(new Set());
 
-  const {data: friends=[], isLoading:loadingFriends} = useQuery({
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
     queryKey: ["friends"],
-    queryFn: getUserFriends
-  })
+    queryFn: getUserFriends,
+  });
 
-  const {data:recommendedUsers=[], isLoading:loadingUsers} = useQuery({
+  const { data: recommendedUsers = [], isLoading: loadingUsers } = useQuery({
     queryKey: ["users"],
-    queryFn: getRecommendedUsers
-  })
+    queryFn: getRecommendedUsers,
+  });
 
-  const {data: outgoingFriendReqs} = useQuery({
-    queryKey:["outgoingFriendreqs"],
+  const { data: outgoingFriendReqs } = useQuery({
+    queryKey: ["outgoingFriendReqs"],
     queryFn: getOutgoingFriendReqs,
-  })
+  });
 
-  const {mutate: sendRequestMutation, isPending} = useMutation({
+  const { mutate: sendRequestMutation, isPending } = useMutation({
     mutationFn: sendFriendRequest,
-    onSuccess: ()=> queryClient.invalidateQueries({queryKey: ["outgoingFriendReqs"]}),
-  })
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] }),
+  });
+
+  useEffect(() => {
+    const outgoingIds = new Set();
+    if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
+      outgoingFriendReqs.forEach((req) => {
+        outgoingIds.add(req.recipient._id);
+      });
+      setOutgoingRequestsIds(outgoingIds);
+    }
+  }, [outgoingFriendReqs]);
+
   return (
-    <div>
+    <div className=''>
 
     </div>
   )
