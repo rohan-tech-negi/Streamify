6de17ff4679df5363a93cf3035e4785fdcd1c4dd
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-14 12:49:53 +0530

    started working on the theme selector to have the access to change the enironment colors

--- frontend/src/components/ThemeSelector.jsx
+++ frontend/src/components/ThemeSelector.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import useThemeStore from '../store/useThemeStore'
+import { PaletteIcon } from 'lucide-react'
+import { THEMES } from '../constants'
 const ThemeSelector = () => {
+    const{theme, setTheme} = useThemeStore()
   return (
-    <div>ThemeSelector</div>
+    <div className='dropdown dropdown-end'>
+        {/* DROPDOWN TRIGGER */}
+        <button tabIndex={0} className='btn btn-ghost btn-circle'>
+            <PaletteIcon className='size-5'></PaletteIcon>
+        </button>
+
+        <div tabIndex={0} className='dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl w-56 broder border-base-content/10 max-h-80 overflow-y-auto'>
+
+            <div className='space-y-1'>
+                {THEMES.map((themeOption)=>{
+                  <button key={themeOption.name}>
+                      
+                  </button>
+                })}
+            </div>
+        </div>
+    </div>
   )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import useThemeStore from '../store/useThemeStore'
+
+
 // import {useThemeStore} from "../store/useThemeStore.js"
 const HomePage = () => {
 
-  const{theme, setTheme} = useThemeStore() 
+
   return (
-    <div>HomePage</div>
+    <div>
+
+    </div>
   )
 }
 
