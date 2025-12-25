94ae2a4b489c94a7a89f4e6ac0d7023f94b68308
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-25 13:42:24 +0530

    completed the mutations of the notificationpage and starting working on the ui of the notificationpage

--- frontend/src/pages/NotificationPage.jsx
+++ frontend/src/pages/NotificationPage.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+// import React from 'react'
+
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { acceptFriendRequest, getFriendRequests } from "../lib/api";
 
 const NotificationPage = () => {
+  const queryClient = useQueryClient();
+
+  const {data:friendRequests, isLoading} = useQuery({
+    queryKey: ["friendRequests"],
+    queryFn: getFriendRequests,
+  })
+
+  const {mutate:acceptRequestsMutation, isPending} = useMutation({
+    mutationFn: acceptFriendRequest,
+    onSuccess: ()=>{
+      queryClient.invalidateQueries({queryKey:["friendRequests"]})
+      queryClient.invalidateQueries({queryKey: ["friend"]})
+    }
+  })
+
+  const incomingRequests = friendRequests?.incomingReqs || []
+  const acceptedRequests = friendRequests?.acceptedReqs || []
   return (
     <div>NotificationPage</div>
   )
