86a2cd9f021f61613cbca38965dcc2433bac55d1
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-26 13:31:22 +0530

    as setting up the backend usability in the chatpage code

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {useParams} from "react-router"
+import {useQuery} from "@tanstack/react-query"
+import {getStreamToken} from "../lib/api.js"
+import useAuthUser from "../hooks/useAuthUser.js"
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
 const ChatPage = () => {
+ const {id: targetUserId} = useParams()
+
+ const [chatClient, setChatClient] = useState(null);
+ const[channel, setChannel] = useState(null);
+ const [loading, setLoading] = useState(true);
+
+ const {authUser} = useAuthUser()
+
+ const {data} = useQuery({
+    queryKey:["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!authUser
+ })
   return (
     <div>ChatPage</div>
   )
