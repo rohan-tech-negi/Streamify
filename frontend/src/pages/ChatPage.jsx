3ac183ecd76a9f095f27075f58f9adb12b9ae584
rohan-tech-negi <rohanwork953@gmail.com>
2025-12-26 13:47:36 +0530

    setup the backend data extraction from the database and created a chatlooader

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useParams} from "react-router"
 import {useQuery} from "@tanstack/react-query"
 import {getStreamToken} from "../lib/api.js"
@@ -12,6 +12,12 @@ import {
   Thread,
   Window,
 } from "stream-chat-react";
+import { StreamChat } from 'stream-chat'
+import {toast} from "react-hot-toast"
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY
+
+
 const ChatPage = () => {
  const {id: targetUserId} = useParams()
 
@@ -21,11 +27,46 @@ const ChatPage = () => {
 
  const {authUser} = useAuthUser()
 
- const {data} = useQuery({
+ const {data:tokenData } = useQuery({
     queryKey:["streamToken"],
     queryFn: getStreamToken,
     enabled: !!authUser
  })
+
+ useEffect(()=>{
+    const initChat = async()=>{
+      if(!tokenData?.token || !authUser) return;
+
+      try {
+        console.log("Initializing stream chat client ... ")
+        const client = StreamChat.getInstance(STREAM_API_KEY);
+
+        await client.connectUser({
+          id: authUser._id,
+          name: authUser.fullName,
+          image: authUser.profilePic,
+        }, tokenData.token)
+
+        const channelId = [authUser._id, targetUserId].sort().join("-");
+
+
+        const currChannel = client.channel("message", channelId, {
+          members: [authUser._id , targetUserId],
+        })
+
+        await currChannel.watch();
+
+        setChatClient(client);
+        setChannel(currChannel)
+      } catch (error) {
+        toast.error("Could not connect to chat. please try again", error);
+      } finally{
+        setLoading(false)
+      }
+    }
+ },[])
+
+ if(loading || !chatClient || !channel) return <ChatLoader></ChatLoader>
   return (
     <div>ChatPage</div>
   )
