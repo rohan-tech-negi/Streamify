6c739d0ed4d0b2151a71d1d9fe52f95bfd76b55d
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-29 15:54:32 +0530

    setup the main server.js for the operations

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,43 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
-import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "../src/routes/chat.route.js"
-import cors from "cors"
-dotenv.config()
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
 
-connectDB()
-const app = express()
-const PORT = process.env.PORT
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-app.use(express.json())
-app.use(cookieParser())
+import { connectDB } from "./lib/db.js";
 
+const app = express();
+const PORT = process.env.PORT;
 
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/chat", chatRoutes)
+const __dirname = path.resolve();
 
-app.listen(PORT,()=>{
-    console.log("the app is up on port 5000")
-})
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
