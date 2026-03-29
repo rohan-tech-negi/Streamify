9e7cef6ffe0df2bbe0f8cecfe0fa24c8ebb18b01
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-29 15:48:55 +0530

    working on improving the streamify project

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,25 +1,18 @@
-import express from "express"
-import { login, logout, signup ,onboard} from "../controller/auth.controller.js"
-import { protectRoute } from "../middleware/auth.middleware.js"
+import express from "express";
+import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post("/signup",signup)
+router.post("/onboarding", protectRoute, onboard);
 
-
-router.post("/login",login);
-
-
-router.post("/logout",logout)
-
-
-router.post("/onboarding",protectRoute, onboard)
-
-
-
-router.get("/me", protectRoute, (req,res)=>{
-    res.status(200).json({success:true, user:req.user})
-})
+// check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,16 +1,26 @@
-import express from "express"
-import { protectRoute } from "../middleware/auth.middleware.js"
-import { getMyfriends, getRecommendedUsers , sendFriendRequest , acceptFriendRequest, getFriendRequest , getOutgoingFriendReqs} from "../controller/user.controller.js"
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  acceptFriendRequest,
+  getFriendRequests,
+  getMyFriends,
+  getOutgoingFriendReqs,
+  getRecommendedUsers,
+  sendFriendRequest,
+} from "../controllers/user.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use(protectRoute)
+// apply auth middleware to all routes
+router.use(protectRoute);
 
+router.get("/", getRecommendedUsers);
+router.get("/friends", getMyFriends);
 
-router.get("/",getRecommendedUsers)
-router.get("/friends", getMyfriends)
-router.post("/friend-request/:id", sendFriendRequest)
-router.post("/friend-request/:id/accept", acceptFriendRequest)
-router.post("/friend-request", getFriendRequest)
-router.post("/outgoing-friend-request", getOutgoingFriendReqs)
+router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+
 export default router;
