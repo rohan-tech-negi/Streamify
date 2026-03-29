b980e81f5a56fa6e5f7a40909c4d036924e596e3
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-29 15:50:10 +0530

    setup the models and the authmiddleware

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,27 +1,29 @@
-import mongoose from "mongoose"
-import bcrypt from "bcryptjs"
-const userSchema = new mongoose.Schema({
-    fullName:{
-        type:String,
-        required:true
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:6
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
     },
-    bio:{
-        type:String,
-        default:""
+    bio: {
+      type: String,
+      default: "",
     },
-    profilePic:{
-        type:String,
-        default:""
+    profilePic: {
+      type: String,
+      default: "",
     },
     nativeLanguage: {
       type: String,
@@ -39,43 +41,33 @@ const userSchema = new mongoose.Schema({
       type: Boolean,
       default: false,
     },
-    friends:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User"
-        }
-    ]
-}, {timestamps:true})
-
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    if(!this.isModified("password")) return next()
-    try {
-        const salt  = await bcrypt.genSalt(10)
-        this.password = await bcrypt.hash(this.password, salt)
-
-        next()
-    } catch (error) {
-        next(error)
-    }
-})
-
-
-
-userSchema.methods.matchPassword = async function(enteredPassword) {
-    const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
-    return isPasswordCorrect;
-}
-
-const User = mongoose.model("User", userSchema);
-
-// pre hook
-
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
   return isPasswordCorrect;
 };
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
